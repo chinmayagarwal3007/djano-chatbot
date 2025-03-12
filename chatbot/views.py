774b2806ab61f3185699c6d65eae548ea572fbab
Chinmay <agarwalchinmay3007@gmail.com>
2025-03-12 08:22:01 +0530

    update

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 def chatbot(request):
+    if request.method == 'POST':
+        print('I am here')
+        message = request.POST.get('message')
+        print(message)
+        response = 'Hi this is my response'
+        return JsonResponse({'message':message, 'response':response}) 
     return render(request, 'chatbot.html')
