8115cec235a5340816d7aab302bba494775abde2
Chinmay <agarwalchinmay3007@gmail.com>
2025-03-12 09:43:46 +0530

    add env

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 import google.generativeai as genai
+import os
+from dotenv import load_dotenv
 
-gemini_api_key = "AIzaSyD96dIsNYmvuH3N0Hy9eIUyzU96ui4vHDs"
+gemini_api_key = os.getenv('gemini_api_key')
 
 genai.configure(api_key=gemini_api_key)
 
@@ -15,6 +17,5 @@ def chatbot(request):
     if request.method == 'POST':
         message = request.POST.get('message')
         response = ask_openai(message)
-        print(response)
         return JsonResponse({'message':message, 'response':response}) 
     return render(request, 'chatbot.html')
