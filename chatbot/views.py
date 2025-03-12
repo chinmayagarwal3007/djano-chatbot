f309ae3b9ee1cc3aa53f3511cc9a1e79e87a743d
Chinmay <agarwalchinmay3007@gmail.com>
2025-03-12 09:32:23 +0530

    add gemini api

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+import google.generativeai as genai
+
+gemini_api_key = "AIzaSyD96dIsNYmvuH3N0Hy9eIUyzU96ui4vHDs"
+
+genai.configure(api_key=gemini_api_key)
+
+def ask_openai(message):
+    model=genai.GenerativeModel("gemini-2.0-flash")
+    response = model.generate_content(message)
+    return response.text
 
 def chatbot(request):
     if request.method == 'POST':
-        print('I am here')
         message = request.POST.get('message')
-        print(message)
-        response = 'Hi this is my response'
+        response = ask_openai(message)
+        print(response)
         return JsonResponse({'message':message, 'response':response}) 
     return render(request, 'chatbot.html')
