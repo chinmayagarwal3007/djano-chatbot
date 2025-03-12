824b92b53ed13c8110d07ab052ad71685e710e1e
Chinmay <agarwalchinmay3007@gmail.com>
2025-03-12 09:49:12 +0530

    fix env

--- chatbot/views.py
+++ chatbot/views.py
@@ -4,7 +4,10 @@ import google.generativeai as genai
 import os
 from dotenv import load_dotenv
 
+load_dotenv()
+
 gemini_api_key = os.getenv('gemini_api_key')
+print(gemini_api_key)
 
 genai.configure(api_key=gemini_api_key)
 
